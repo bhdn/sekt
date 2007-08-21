a75bf88f42ca69bc2b05d6d612f193d831b683a9
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2007-08-21 18:23:21 -0300

    Added CVE abstractions for the entry and of the storage layers.

--- mdv/kernel/ktasks.py
+++ mdv/kernel/ktasks.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 
+#TODO use yaml as configuration source
+#TODO move all string constants to configuration
+
 import sys
 import os
 import optparse
 import shelve
+import re
+import urllib
+import logging
+import zipfile
+
+import bugz
+
+log = logging.getLogger("ktasks")
+
+class Error(Exception):
+    pass
+
+class TicketError(Error):
+    pass
 
 class UnknowTicket:
 
@@ -29,21 +46,86 @@ class TicketCache:
         pass
 
 
+def fetch_url(url):
+    """Small layer just to allow further enhacements"""
+    log.info("fetching URL: %s" % url)
+    return urllib.urlopen(url).read()
+
+
+class CVESource:
+
+    def __init__(self, dbpath):
+        self.root = "./tree"
+        self.dbpath = dbpath
+        self._zipfile = zipfile.ZipFile(dbpath, "r")
+
+    def _get_path(self, cveid):
+        cve_, year, number = cveid.split("-")
+        return os.path.join(self.root, year, number[:2], cveid)
+
+    def _get_xml(self, cveid):
+        path = self._get_path(cveid)
+        xml = self._zipfile.read(path)
+        return xml
+
+    def _get_cve_from_xml(self, xml):
+        return CVE.from_mitre_xml(xml)
+
+    def get_cve(self, cveid):
+        xml = self._get_xml(cveid)
+        cve = self._get_cve_from_xml(xml)
+
+
+class CVE:
+
+    class NoDescriptionYet:
+        pass
+
+    cveid = None
+    references = None
+    description = None
+    phase = None
+
+    def __init__(self, cveid):
+        self.cveid = cveid
+
+    @classmethod
+    def from_mitre_xml(klass, xml):
+        root = xml.getroot()
+        cveid = root.attrib["name"]
+        cve = CVE(cveid) #FIXME we already have cveid!
+        cve.description = root.find(".//desc").text
+        cve.references = [{
+                "source": ref.attrib.get("source"),
+                "url": ref.attrib.get("url"),
+                "descr": ref.text }
+            for ref in root.findall(".//refs/ref")]
+        # don't parse comments, as apparently we wont' need them
+        cve.phase = root.find(".//phase").text
+        return cve
+
+
 class SecurityTicket(Ticket):
 
-    _ticket = None
     cve = None
-    sources = None
     url = None
     valid = None
     release_status = None
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, ):
         Ticket.__init__(self, *args, **kwargs)
         self._parse_ticket()
 
     def _parse_ticket(self):
-        pass
+        #TODO move this regexp to configuration
+        cvere = r"SECURITY\s+ADVISORY:?\s+(?P<cve>CVE-....-....)"
+        found = re.match(cvere, self.title)
+        if found is None:
+            raise TicketError, "bad ticket title: %r, must match %s" % \
+                    (self.title, cvere)
+        self.cveid = found.group("cve")
+        self.
+
 
     def __getattr__(self, name):
         return getattr(self._ticket, name)
@@ -54,7 +136,7 @@ class TicketSource:
     def __init__(self, config):
         self.config = config
 
-    def search(self):
+    def search(self, ):
         pass
 
     def _make_ticket(self):
